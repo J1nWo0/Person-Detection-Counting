a5973ccd99822bbb897d7be3404be270fedfce52
Sherwin S. Laguidao <sherwin.laguidao533@gmail.com>
2023-12-09 17:05:05 +0800

    creating a comment in line 8 and 20

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ from human_detection_webcam import *
 
 
 
+# For Uploading a video files
 a1=[(312,388),(289,390),(474,469),(497,462)]
 a2=[(279,392),(250,397),(423,477),(454,469)]
 
@@ -16,7 +17,7 @@ algo = Algorithm_Count(a1, a2)
 algo.counting(in_video_path)
 
 '''
-
+# For see the webcam
 algo = Algorithm_Detection()
 
 algo.detectPeople()
