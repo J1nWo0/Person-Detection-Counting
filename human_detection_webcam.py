f8eb652a2703f950fd4990b35a9ec13de1db4210
Sherwin S. Laguidao <sherwin.laguidao533@gmail.com>
2023-12-15 17:43:09 +0800

    Cleaning some code

--- human_detection_webcam.py
+++ human_detection_webcam.py
@@ -1,25 +1,27 @@
 # %%
 import cv2
+import cvzone
 from ultralytics import YOLO
-
+from human_counting import Color 
 
 class Algorithm_Detection:
     def detectPeople(self):
+        #color = Color()
         #model = YOLO('datasets\\weights\\best.pt')
         model = YOLO('yolo-Weights\yolov8n.pt')
         cap = cv2.VideoCapture(0)
-
+        cap.set(3, 1040)
+        cap.set(4, 680)
         while True:
             ret, frame = cap.read()
-            if not ret:
-                break
-
-            frame = cv2.resize(frame, (750, 590))
-            results = model.track(frame, persist=True, conf=0.5)
+            if not ret: break
+            frame = cv2.resize(frame, (1040, 680))
+            results = model.track(frame, persist=True, conf=0.5, classes=[0])
             frame = results[0].plot()
-            cv2.imshow('Webcam Detection', frame)
+            cvzone.putTextRect(frame,str('Pres [q] to quit'), (30,30), 1,1, Color().text1(), Color().text2())
+            cv2.imshow('Webcam', frame)
 
-            if cv2.getWindowProperty('Webcam Detection', cv2.WND_PROP_VISIBLE) < 1:
+            if cv2.waitKey(1) == ord('q'): #cv2.getWindowProperty('Webcam', cv2.WND_PROP_VISIBLE) < 1:
                 break
 
         cap.release()
